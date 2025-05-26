129d6881b73fb89a2c8818c610b8afb5daf5b646
Abdul Malek <abmalek586@gmail.com>
2025-05-26 23:01:11 +0600

    Added javacript

---
+++ js-basics-calculator/script.js
@@ -0,0 +1,19 @@
+
+const display = document.getElementById("display");
+
+function addToDisplay(input){
+    display.value += input;
+}
+
+function showResult(){
+    try{
+        display.value = eval(display.value);   
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
+
+function clearDisplay(){
+    display.value = "";
+}
